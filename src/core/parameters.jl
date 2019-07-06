1d75fd53066dd7b6a2c5a5a649d6527a680f0a68
grahamstark <graham.stark@virtual-worlds.biz>
2019-07-06 20:34:28 +0100

    some params

--- src/core/parameters.jl
+++ src/core/parameters.jl
@@ -1,2 +1,16 @@
 # 
 include( "definitions.jl" )
+
+mutable struct IncomeTax
+
+end
+
+mutable struct NationalInsurance
+
+end
+
+mutable struct TaxBenefitSystem
+   name :: AbstractString
+   it :: IncomeTax
+   ni :: NationalInsurance
+end
