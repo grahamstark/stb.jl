cb9bfbd998fe66e7efadf1c97f9283a22e40a542
grahamstark <graham.stark@virtual-worlds.biz>
2019-09-04 09:19:55 +0100

    enums 1/2 way

--- src/data_mapping/create_enums.jl
+++ src/data_mapping/create_enums.jl
@@ -18,18 +18,29 @@ function make_from_frs()
     Activities_Of_Daily_Living_Bool_Array
     Council_Tax_Band_Type CTBAND Houshol
 
-    Employment_Status_ILO_Definition :: EMPSTATI
-    Ethnic_Group_Type :: ETHGRP [ETHGRPS - 2016/2017 only]
-    Gender_Type :: SEX
-    Health_Status_Self_Reported :: HEATHAD / HEATHCH adu
-    Marital_Status_Type :: MARITAL
-    Msc_Data_Enums.Qualification_Type :: DVHIQUAL
-    Region_Type :: GVTREGN
-    Socio_Economic_Grouping_Type :: NSSEC
-    Standard_Industrial_Classification_2007 :: SIC
-    Standard_Occupational_Classification :: SC2010HD
-    Tenure_Type :: tentyp2
+#    Employment_Status_ILO_Definition :: EMPSTATI
+#    Ethnic_Group_Type :: ETHGRP [ETHGRPS - 2016/2017 only]
+#    Gender_Type :: SEX
+#    Health_Status_Self_Reported :: HEATHAD / HEATHCH adu
+#    Marital_Status_Type :: MARITAL
+#    Msc_Data_Enums.Qualification_Type :: DVHIQUAL
+#    Region_Type :: GVTREGN
+#    Socio_Economic_Grouping_Type :: NSSEC
+#    Standard_Industrial_Classification_2007 :: SIC
+#    Standard_Occupational_Classification :: SC2010HD
+#    Tenure_Type :: tentyp2
 
+    println( make_enumerated_type( allv[:empstati], true, true )) # employment_status_ilo_definition
+    println( make_enumerated_type( allv[:ethgrp], true, true )) # ethnic_group_type[ethgrps - 2016/2017 only]
+    println( make_enumerated_type( allv[:sex], true, true )) # gender_type
+    println( make_enumerated_type( allv[:heathad], true, true )) # health_status_self_reported/ heathch adu
+    println( make_enumerated_type( allv[:marital], true, true )) # marital_status_type
+    println( make_enumerated_type( allv[:dvhiqual], true, true )) # msc_data_enums.qualification_type
+    println( make_enumerated_type( allv[:gvtregn], true, true )) # region_type
+    println( make_enumerated_type( allv[:nssec], true, true )) # socio_economic_grouping_type
+    println( make_enumerated_type( allv[:sic], true, true )) # standard_industrial_classification_2007
+    println( make_enumerated_type( allv[:sc], true, true )) # standard_occupational_classification2010hd
+    println( make_enumerated_type( allv[:tentyp], true, true )) # tenure_type2
 
 
 end
