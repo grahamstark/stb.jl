fd40038ad8b61d5d9e0a9a19a3832ba40d902498
Graham Stark <graham.stark@virtual-worlds.biz>
2019-09-14 22:27:44 +0100

    Initial version.

---
+++ test/mappingtests.jl
@@ -0,0 +1,15 @@
+using Test
+
+
+include( "../src/core/calculations.jl" )
+include( "../src/core/results.jl" )
+include( "../src/core/household.jl" )
+include( "../src/core/parameters.jl" )
+include( "../src/persist/household_frames.jl" )
+include( "../src/persist/result_frames.jl" )
+
+@testset begin
+
+
+
+end
