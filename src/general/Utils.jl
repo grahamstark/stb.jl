1b1d0b552807eef46a9862967963959ba3ca3f34
Graham Stark <graham.stark@virtual-worlds.biz>
2019-09-18 23:28:43 +0100

    mini server stuff with BC

--- src/general/Utils.jl
+++ src/general/Utils.jl
@@ -2,87 +2,102 @@ module Utils
 
 using Base.Unicode
 
-      export @exported_enum, qstrtodict, pretty, basiccensor
+export @exported_enum, qstrtodict, pretty, basiccensor, get_if_set
 
 
-      """
-      parse an html query string like "sex=male&joe=22&bill=21342&z=12.20"
-      into a dict. If the value looks like a number, it's parsed into either an Int64 or Float64
-      """
-      function qstrtodict( query_string :: AbstractString ) :: Dict{AbstractString,Any}
-        d = Dict{AbstractString,Any}()
-        as = split( query_string, "&")
-        for a in as
-          aa = split( a, "=")
-          k = aa[1]
-          v = aa[2]
-          try
-            v = parse( Int64, v )
-          catch
-            try
-              v = parse( Float64, v )
-            catch
-            end
-          end
-          d[k] = v
+function get_if_set(key, dict::Dict, default)
+  if haskey(dict, key)
+    return dict[key]
+  end
+  default
+end
+
+"""
+parse an html query string like "sex=male&joe=22&bill=21342&z=12.20"
+into a dict. If the value looks like a number, it's parsed into either an Int64 or Float64
+"""
+function qstrtodict(query_string::AbstractString)::Dict{AbstractString,Any}
+  d = Dict{AbstractString,Any}()
+  strip(query_string)
+  if (query_string == "")
+    return d
+  end
+  as = split(query_string, "&")
+  for a in as
+    try
+      aa = split(a, "=")
+      k = aa[1]
+      v = aa[2]
+      try
+        v = parse(Int64, v)
+      catch
+        try
+          v = parse(Float64, v)
+        catch
         end
-        d
       end
+      d[k] = v
+    catch
+
+    end
+  end
+  d
+end
 
 
-      """
-      returns the string converted to a form suitable to be used as (e.g.) a Symbol,
-      with leading/trailing blanks removed, forced to lowercase, and with various
-      characters replaced with '_' (at most '_' in a run).
-      """
-      function basiccensor( s :: AbstractString ) :: AbstractString
-             s = strip( lowercase( s ))
-             s = replace( s, r"[ \-,\t–]" => "_" )
-             s = replace( s, r"[=\:\)\('’‘]" => "" )
-             s = replace( s,  r"[\";:\.\?\*”“]" => "" )
-             s = replace( s,  r"_$"=> "" )
-             s = replace( s,  r"^_"=> "" )
-             s = replace( s,  r"^_"=> "" )
-             s = replace( s,  r"\/"=> "_or_" )
-             s = replace( s,  r"\&"=> "_and_" )
-             s = replace( s,  r"\+"=> "_plus_" )
-             s = replace( s,  r"_\$+$"=> "" )
-             if occursin( r"^[\d].*", s )
-                      s = string("v_", s ) # leading digit
-             end
-             s = replace( s,  r"__"=> "_" )
-             s = replace( s, r"^_" => "" )
-             s = replace( s, r"_$" => "" )
-             return s
-      end
+"""
+returns the string converted to a form suitable to be used as (e.g.) a Symbol,
+with leading/trailing blanks removed, forced to lowercase, and with various
+characters replaced with '_' (at most '_' in a run).
+"""
+function basiccensor(s::AbstractString)::AbstractString
+  s = strip(lowercase(s))
+  s = replace(s, r"[ \-,\t–]" => "_")
+  s = replace(s, r"[=\:\)\('’‘]" => "")
+  s = replace(s, r"[\";:\.\?\*”“]" => "")
+  s = replace(s, r"_$" => "")
+  s = replace(s, r"^_" => "")
+  s = replace(s, r"^_" => "")
+  s = replace(s, r"\/" => "_or_")
+  s = replace(s, r"\&" => "_and_")
+  s = replace(s, r"\+" => "_plus_")
+  s = replace(s, r"_\$+$" => "")
+  if occursin(r"^[\d].*", s)
+    s = string("v_", s) # leading digit
+  end
+  s = replace(s, r"__" => "_")
+  s = replace(s, r"^_" => "")
+  s = replace(s, r"_$" => "")
+  return s
+end
 
 
-      """
-      a_string_or_symbol_like_this => "A String Or Symbol Like This"
-      """
-     function pretty( a )
-           s = string( a )
-           s = strip( lowercase( s ))
-           s = replace( s, r"[_]" => " " )
-           Unicode.titlecase( s )
-     end
+"""
+a_string_or_symbol_like_this => "A String Or Symbol Like This"
+"""
+function pretty(a)
+  s = string(a)
+  s = strip(lowercase(s))
+  s = replace(s, r"[_]" => " ")
+  Unicode.titlecase(s)
+end
 
 
-     """
-      macro to define an enum and automatically
-      add export statements for its elements
-      see: https://discourse.julialang.org/t/export-enum/5396
-     """
-      macro exported_enum(name, args...)
-          esc(quote
-              @enum($name, $(args...))
-              export $name
-              for a in $args
-                   local av = string(a)
-                   :(export $av)
-              end
+"""
+ macro to define an enum and automatically
+ add export statements for its elements
+ see: https://discourse.julialang.org/t/export-enum/5396
+"""
+macro exported_enum(name, args...)
+  esc(quote
+    @enum($name, $(args...))
+    export $name
+    for a in $args
+      local av = string(a)
+      :(export $av)
+    end
               # $([:(export $arg) for arg in args]...)
-              end)
-      end
+  end)
+end
 
 end # module
--- src/web/server.jl
+++ src/web/server.jl
@@ -9,83 +9,100 @@ using Example_Household_Getter
 using Model_Household
 using Utils
 using MiniTB
+using TBComponents
 
+
+
+function local_makebc(pers::MiniTB.Person, params::Parameters)::Array{Float64,2}
+
+   function getnet(gross::Float64)::Float64
+      pers.wage = gross
+      rc = calculate(pers, params)
+      return rc[:netincome]
+   end
+   bc = TBComponents.makebc(getnet)
+   return pointstoarray(bc)
+end
+
+const DEFAULT_BC = local_makebc(MiniTB.DEFAULT_PERSON, MiniTB.DEFAULT_PARAMS)
+
+# it_allow=500.0&it_rate_1=0.25&it_rate_2=0.5&it_band=10000&benefit1=150.0&benefit2=60.0&ben2_l_limit = 200.03&ben2_u_limit = 300.20
+# it_allow=0&it_rate_1=0&it_rate_2=0&it_band=0&benefit1=0&benefit2=0.0&ben2_l_limit=0&ben2_u_limit=0
+function local_makebc(req)
+   querydict = req[:parsed_querystring]
+   tbparams = MiniTB.DEFAULT_PARAMS
+   tbparams.it_allow = get_if_set("it_allow", querydict, tbparams.it_allow)
+   tbparams.it_rate[1] = get_if_set("it_rate_1", querydict, tbparams.it_rate[1])
+   tbparams.it_rate[2] = get_if_set("it_rate_2", querydict, tbparams.it_rate[2])
+   tbparams.it_band[1] = get_if_set("it_band", querydict, tbparams.it_band[1])
+
+   tbparams.benefit1 = get_if_set("benefit1", querydict, tbparams.benefit1)
+   tbparams.benefit2 = get_if_set("benefit2", querydict, tbparams.benefit2)
+   tbparams.ben2_l_limit = get_if_set("ben2_l_limit", querydict, tbparams.ben2_l_limit)
+   tbparams.ben2_u_limit = get_if_set("ben2_u_limit", querydict, tbparams.ben2_u_limit)
+
+   bc = local_makebc(MiniTB.DEFAULT_PERSON, tbparams)
+   res = (base = DEFAULT_BC, changed = bc)
+   JSON.json(res)
+end
+
+function get_hh(hdstr::AbstractString)
+   hid = parse(Int64, hdstr)
+   JSON.json(FRS_Household_Getter.get_household(hid))
+end
+
+function addqstrdict(app, req)
+   req[:parsed_querystring] = qstrtodict(req[:query])
+   return app(req)
+end
+
+function paramtest(req)
+   JSON.json(req[:query])
+   JSON.json(req)
+end
+
+# Better error handling
+function errorCatch(app, req)
+   try
+      app(req)
+   catch e
+      println("Error occured!")
+
+      io = IOBuffer()
+      showerror(io, e)
+      err_text = takebuf_string(io)
+      println(err_text)
+      resp = withHeaders(JSON.json(Dict("message" => err_text, "error" => true)), req)
+      resp[:status] = 500
+      return resp
+   end
+end
+
+# ourstack = stack(Mux.todict, errorCatch, Mux.splitquery, Mux.toresponse) # from DiffEq
+
+@app dd226 = (
+   Mux.defaults,
+   addqstrdict,
+# Mux.splitquery,
+   page(respond("<h1>OU DD226 TB Model</h1>")),
+   page("/hhld/:hid", req -> get_hh((req[:params][:hid]))),
+   page("/paramtest", req -> paramtest(req)),
+   page("/bc", req -> local_makebc(req)),
+   Mux.notfound(),
+)
 
 println("starting server")
 rc = @timed begin
-  example_names = Example_Household_Getter.initialise()
-  num_households = FRS_Household_Getter.initialise()
+   example_names = Example_Household_Getter.initialise()
+  # num_households = FRS_Household_Getter.initialise()
 end
 mb = trunc(Integer, rc[3] / 1024^2)
 println("loaded data; load time $(rc[2]); memory used $(mb)mb;\nready...")
 
 
-function makebc(pers::Person, params::Parameters)::BudgetConstraint
-
-  function getnet(gross::Float64)::Float64
-    persedit = modifiedcopy(pers, wage = gross)
-        # println( "getnet; made person $persedit")
-    rc = calculate(persedit, params)
-    return rc[:netincome]
-  end
-
-  bc = makebc(getnet)
-
-  return bc
-end
-
-function makebc(req)
-  bc = makebc(DEFAULT_PERSON, DEFAULT_PARAMS)
-  JSON.json(bc)
-end
-
-function get_hh(hdstr::AbstractString)
-  hid = parse(Int64, hdstr)
-  JSON.json(FRS_Household_Getter.get_household(hid))
-end
-
-function addqstrdict(app, req)
-  req[:parsed_querystring] = qstrtodict(req[:query])
-  return app(req)
-end
-
-function paramtest(req)
-  JSON.json(req[:query])
-  JSON.json(req)
-end
-
-# Better error handling
-function errorCatch(app, req)
-  try
-    app(req)
-  catch e
-    println("Error occured!")
-
-    io = IOBuffer()
-    showerror(io, e)
-    err_text = takebuf_string(io)
-    println(err_text)
-    resp = withHeaders(JSON.json(Dict("message" => err_text, "error" => true)), req)
-    resp[:status] = 500
-    return resp
-  end
-end
-
-# ourstack = stack(Mux.todict, errorCatch, Mux.splitquery, Mux.toresponse) # from DiffEq
-
-@app test = (
-  Mux.defaults,
-  addqstrdict,
-# Mux.splitquery,
-  page(respond("<h1>OU DD226 TB Model</h1>")),
-  page("/hhld/:hid", req -> get_hh((req[:params][:hid]))),
-  page("/paramtest", req -> paramtest(req)),
-  page("/bc", req -> makebc(req)),
-  Mux.notfound(),
-)
 
 port = 8000
 if length(ARGS) > 0
-  port = parse(Int64, ARGS[1])
+   port = parse(Int64, ARGS[1])
 end
-@sync serve(test, port)
+@sync serve(dd226, port)
