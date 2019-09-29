65236589fb4a3647912976531892d276546abe0b
grahamstark <graham.stark@virtual-worlds.biz>
2019-09-29 12:00:40 +0100

    1 thread ????

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd $OU_HOME
-$JULIA/bin/julia --procs=auto --optimize=3 --track-allocation=none --check-bounds=no --code-coverage=none src/web/server.jl 
+# --procs=auto
+$JULIA/bin/julia --startup-file=yes --optimize=3 --track-allocation=none --check-bounds=no --code-coverage=none src/web/server.jl 
