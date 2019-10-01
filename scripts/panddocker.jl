f11732dd07a1829783360d0d2ddb65c8552c88c3
Graham Stark <graham.stark@virtual-worlds.biz>
2019-10-01 21:30:15 +0100

    Creating multiple pages

---
+++ scripts/panddocker.jl
@@ -0,0 +1,58 @@
+opts = Dict(
+    "css" => ["https://learn2.open.ac.uk/theme/styles.php/osep/1569489122_1567580537/all", "/css/oustb.css"],
+    "include-in-header" => [
+        "/js/jquery.js",
+        "/js/jquery-ui/jquery-ui.min.js",
+        "/js/numeral.js",
+        "/js/mustache.min.js",
+        "/js/stb-support.js",
+        "/js/vega-lite.min.js",
+        "/js/vega.js",
+        "/js/vega-embed.min.js"
+    ],
+    "from" => "markdown+smart",
+    "section-divs" => true,
+    "standalone" => true,
+    "number-sections" => true,
+    "bibliography" => "includes/dd226.bib",
+    "data-dir" => "/home/graham_s/pandoc_data/",
+    "default-image-extension"=>"svg",
+    "csl" => "/home/graham_s/pandoc_data/chicago-note-bibliography.csl",
+    "to" => "html5"
+
+);
+
+function makeoptstring( opts :: Dict, format=nothing ) :: AbstractString
+    oldformat = opts["to"]
+    if format !== nothing
+         opts["to"] = format
+    end
+    s = ""
+    for (k,v) in opts
+        if v === true
+            v = ""
+        end
+        if length(k) == 1
+            k = "-$k"
+        else
+            k = "--$k"
+        end
+        if ! (typeof( v ) <: Array)
+            v = [v]
+        end
+        for ve in v
+            if ve !== ""
+                s *= " $k=$ve"
+            else
+                s *= " $k "
+            end
+        end
+    end
+    opts["to"] = oldformat
+    s
+end
+
+opts["include-after-body"]=["includes/main_form.html"]
+opts["include-before-body"]=["includes/main_output.html"]
+
+`/usr/bin/pandoc`
