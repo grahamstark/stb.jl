f532dd7a38e568e1cd2a43e2365da1de00375092
Graham Stark <graham.stark@virtual-worlds.biz>
2019-10-02 16:19:52 +0100

    templates and stuff

--- scripts/panddocker.jl
+++ scripts/panddocker.jl
@@ -1,30 +1,33 @@
-opts = Dict(
-    "css" => ["https://learn2.open.ac.uk/theme/styles.php/osep/1569489122_1567580537/all", "/css/oustb.css"],
-    "include-in-header" => [
-        "/js/jquery.js",
-        "/js/jquery-ui/jquery-ui.min.js",
-        "/js/numeral.js",
-        "/js/mustache.min.js",
-        "/js/stb-support.js",
-        "/js/vega-lite.min.js",
-        "/js/vega.js",
-        "/js/vega-embed.min.js"
-    ],
-    "from" => "markdown+smart",
-    "section-divs" => true,
-    "standalone" => true,
-    "number-sections" => true,
-    "bibliography" => "includes/dd226.bib",
-    "data-dir" => "/home/graham_s/pandoc_data/",
+MD_DIR = "/home/graham_s/OU/DD226/docs/sections/"
+OUT_DIR = "/var/www/ou/stb/"
+INCLUDE_DIR = "/home/graham_s/VirtualWorlds/projects/ou/stb.jl/web/includes/"
+PANDOC_DIR = "/home/graham_s/pandoc_data/"
+NullableString = Union{Missing,AbstractString}
+
+const DEFAULT_OPTS = Dict(
+    "data-dir"          => PANDOC_DIR,
+    "css"               =>
+            [
+                "https://learn2.open.ac.uk/theme/styles.php/osep/1569489122_1567580537/all",
+                "/css/oustb.css"
+            ],
+    "include-in-header" => [ "$INCLUDE_DIR/ou-js-headers.html"],
+    "from"              => "markdown+smart",
+    "section-divs"      => true,
+    "standalone"        => true,
+    "number-sections"   => true,
+    "bibliography"      => "DD226.bib",
     "default-image-extension"=>"svg",
-    "csl" => "/home/graham_s/pandoc_data/chicago-note-bibliography.csl",
-    "to" => "html5"
+    "csl"               => "$PANDOC_DIR/chicago-note-bibliography.csl",
+    "to"                => "html5",
+    "filter"            => "pandoc-citeproc",
+    "template"          => "$INCLUDE_DIR/ou-template.html"
 
 );
 
-function makeoptstring( opts :: Dict, format=nothing ) :: AbstractString
+function makeoptstring( opts :: Dict, format=missing ) :: AbstractString
     oldformat = opts["to"]
-    if format !== nothing
+    if ! ismissing(format)
          opts["to"] = format
     end
     s = ""
@@ -42,7 +45,7 @@ function makeoptstring( opts :: Dict, format=nothing ) :: AbstractString
         end
         for ve in v
             if ve !== ""
-                s *= " $k=$ve"
+                s *= " $k $ve"
             else
                 s *= " $k "
             end
@@ -52,13 +55,45 @@ function makeoptstring( opts :: Dict, format=nothing ) :: AbstractString
     s
 end
 
-opts["include-after-body"]=["includes/main_form.html"]
-opts["include-before-body"]=["includes/main_output.html"]
+function addone(
+    pos,
+    count,
+    title::String,
+    output :: NullableString,
+    form::NullableString,
+    content::AbstractString,
+    prev_content::NullableString,
+    next_content::NullableString )
 
-`/usr/bin/pandoc `
+    opts = deepcopy( DEFAULT_OPTS )
+    includes = []
+    if ! ismissing(output)
+        push!( includes, "$(INCLUDE_DIR)/$(output).html" )
+    end
+    if ! ismissing(form)
+        push!( includes, "$(INCLUDE_DIR)/$(form).html" )
+    end
+    if pos > 1
+        opts["number-offset"]=(pos-1)
+    end
+    opts["include-before-body"] = includes
+    forward = ""
+    backward = ""
+    if ! ismissing(prev_content )
+        backward="<a href='$(next_content).html'>prev</a>"
+    end
+    if ! ismissing(prev_content )
+        forward="<a href='$next_content).html'>prev</a>"
+    end
+    nav = "<footer><nav>$backward $forward</nav></footer>"
+    opts["include-after-body"] = nav
+    opts["o"] = "$(OUT_DIR)$(content).html"
+    optstr = makeoptstring( opts )
+    optstr *= " $(MD_DIR)$(content).md"
 
-MD_DIR = "/home/graham_s/OU/DD226/docs/sections/"
-OUT_DIR = "/var/www/ou/stb/"
-INCLUDE_DIR = "/home/graham_s/VirtualWorlds/projects/ou/stb.jl/web/"
+    cmd=`/usr/bin/pandoc $optstr`
+    println( cmd )
+    run( cmd )
+end
 
-cmd=`/usr/bin/pandoc`
+addone( 1, 2, "Introduction",missing,missing,"intro",missing,missing)
