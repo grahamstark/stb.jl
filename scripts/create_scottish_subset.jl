4dbc9304d96fcd3a61b071460f23888263f55667
grahamstark <graham.stark@virtual-worlds.biz>
2019-10-03 07:25:52 +0100

    write statements

--- scripts/create_scottish_subset.jl
+++ scripts/create_scottish_subset.jl
@@ -9,7 +9,7 @@ hh_dataset = CSV.File("$(MODEL_DATA_DIR)/$(household_name).tab", delim='\t' ) |>
 people_dataset = CSV.File("$(MODEL_DATA_DIR)/$(people_name).tab", delim='\t') |> DataFrame
 
 scottish_hhlds = hh_dataset[(hh_dataset.region .== 299999999),:]
-scottish_people = join(people_dataset,scottish_hhlds,on=[:hid,:data_year],kind = :semi)
+scottish_people = join(people_dataset, scottish_hhlds,on=[:hid,:data_year],kind = :semi)
 
-CSV.write("$(MODEL_DATA_DIR)model_households_scotland.tab", model_households, delim = "\t")
-CSV.write("$(MODEL_DATA_DIR)model_people_scotland.tab", model_people, delim = "\t")
+CSV.write("$(MODEL_DATA_DIR)model_households_scotland.tab", scottish_hhlds, delim = "\t")
+CSV.write("$(MODEL_DATA_DIR)model_people_scotland.tab", scottish_people, delim = "\t")
