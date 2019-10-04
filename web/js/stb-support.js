acfd06bf2ed35854edaf4058a220c443dc0832bf
Graham Stark <graham.stark@virtual-worlds.biz>
2019-10-04 17:53:50 +0100

    mrts

--- web/js/stb-support.js
+++ web/js/stb-support.js
@@ -124,13 +124,14 @@ stb.createMarginalRates = function( result ){
             over75_change += result.metr_histogram[2][i];
         }
     }
-    over75 /= tot
-    over75_change /= tot
+    over75 /= tot;
+    over75_change /= tot;
     var view = {
         av_marg_str: numeral(100.0*result.avg_metr[1]).format( '0,0')+"%",
         av_marg_change_str:numeral(100.0*result.avg_metr[2]).format( '0,0'),
         udclass: stb.propToString( result.avg_metr[2]),
         over75: numeral(100.0*over75).format( '0,0')+"%"
+        over75_change: numeral(100.0*over75_change).format( '0,0')
     }
     view.arrow = ARROWS_2[view.udclass];
     var output = Mustache.render(
