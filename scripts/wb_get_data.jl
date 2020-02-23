e71a5d54c75fe404c5b05b9299ecf058be1418eb
grahamstark <graham.stark@virtual-worlds.biz>
2020-02-23 09:03:57 +0000

    start of stress-tester

--- scripts/wb_get_data.jl
+++ scripts/wb_get_data.jl
@@ -8,23 +8,33 @@ using Base.Filesystem
 function getdata( rate1:: Real, rate2 :: Real ) :: Array{Any,1}
     url =
         "https://oustb.virtual-worlds.scot/oustb/stb/?it_allow=12500&it_rate_1=$rate1&it_rate_$rate2&it_band=50000"
+    println( "fetching from URL $url" )
     resp = HTTP.request( "GET", url )
+
     json = JSON.parse(join((map(Char,resp.body))))
     return json;
 end
 
-function doRunBatch( )
+function doRunBatch( max :: Integer )
     n = 0
     for r1 in 1:30
-        for r2 in 30:50
+        for r2 in 40:50
+            println( "getting data r1=$r1 r2=$r2 " )
             json = getdata( r1, r2 )
-            if n % 100 == 0
+            if n % 1 == 0
                 print( json )
             end
             n += 1
-    end
+            if n > max
+                break
+            end
+        end # rand2
+    end # rand1
+end # func doRunBatch
+
+function doall( threadno :: Integer )
+    println(" at start" )
+    doRunBatch(1)
 end
 
-function doall( start )
-    doRunBatch()
-end
+doall(1)
